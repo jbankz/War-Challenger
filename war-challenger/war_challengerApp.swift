e5fb079133fbb119fc0570c533860f283ff491f6
JayCode <vsquare396@gmail.com>
2022-09-30 15:09:40 +0100

    Initial Commit

---
+++ war-challenger/war_challengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengerApp.swift
+//  war-challenger
+//
+//  Created by JayCode on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
